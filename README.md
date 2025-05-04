633a85dc064a7185e67a7c678799888c6bdb65ae
Xinran_RUAN <ruanxinran@me.com>
2025-05-04 17:49:44 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,109 @@
 # AP-DEM_RareMutationLimit
 This is the joint work with Weijie Huang.
 
+## The Model
+We aim to solve the model:
+$$
+\left\{
+\begin{aligned}
+&\varepsilon\partial_t n_\varepsilon -D(\theta)\Delta_x n_\varepsilon -\varepsilon^2\Delta_\theta n_\varepsilon = n_\varepsilon(K(x)-\rho_\varepsilon(x)),\\
+&\rho_\varepsilon = \int_0^1n_\varepsilon(x,\theta) d\theta
+\end{aligned}
+\right.
+$$
+with Neumann BC in $x$ and periodic BC in $\theta$. 
+
+We assume that $K\in C^1(\Omega)$ and $D\in C^1([0,1))$ satisfying 
+$$
+K(x)\ge K_m>0, \quad D(\theta)\ge D(\theta_m):=D_m>0.
+$$
+
+### Theoretical results:
+There are two limits of interest, namely $\varepsilon\to 0$ and $t\to \infty$. 
+
+As $t\to\infty$, it will reach the steady state of  
+$$
+	\begin{cases}
+	\begin{aligned}
+&-D(\theta)\Delta_x n_\varepsilon -\varepsilon^2\Delta_\theta n_\varepsilon = n_\varepsilon(K(x)-\rho_\varepsilon(x)),\\
+&\rho_\varepsilon = \int_0^1n_\varepsilon(x,\theta) d\theta
+	\end{aligned}
+	\end{cases}
+$$
+
+And then we let $\varepsilon\to0$, in the sense of distributions, it can be proved that 
+$$
+	n_\varepsilon(x,\theta) \to N_m(x) \delta(\theta-\theta_m), \quad \rho_\varepsilon \to N_m,
+$$ 
+where $N_m$ is the positive solution of the nonlinear Fisher-type stationary problem
+$$
+	\begin{cases}
+	\begin{aligned}
+&-D_m\Delta_x N_m  = N_m(K(x)-N_m),\\
+& \frac{\partial}{\partial \nu} N_m = 0 \text{ on } \partial\Omega.
+	\end{aligned}
+	\end{cases}
+$$
+Moreover, 
+$$
+	\varepsilon \ln n_\varepsilon(x,\theta) \to u
+$$
+uniformly in $x$ and $\theta$, where $u$ is the unique 1-periodic **viscosity** solution to 
+$$
+\begin{cases}
+\begin{aligned}
+&-|\nabla_\theta u|^2 = -H(\theta, \rho), \\
+& \max_\theta \, u(\theta) = 0, 
+\end{aligned}
+\end{cases}
+$$
+where $H(\theta,\rho)$ is the eigenvalue corresponding to the positive eigenfunction of 
+$$
+	\begin{cases}
+	\begin{aligned}
+&-D(\theta)\Delta_x N  = N(K(x)-\rho) + N H(\theta,\rho),\\
+& \frac{\partial}{\partial \nu} N = 0 \text{ on } \partial\Omega.
+	\end{aligned}
+	\end{cases}
+$$
+
+
+## Design of an A-P scheme
+
+### Reformulation via WKB
+
+Set $n_\varepsilon(x,\theta) = W_\varepsilon(x,\theta) e^{u(\theta)/\varepsilon}$, then
+$$
+\begin{aligned}
+&\partial_t n_\varepsilon = \partial_t W_\varepsilon e^{u/\varepsilon} + W_\varepsilon e^{u/\varepsilon}\dfrac{\partial_t u}{\varepsilon},\\
+&\nabla_x n_\varepsilon = \nabla_x W_\varepsilon ~e^{u/\varepsilon},\\
+&\Delta_x n_\varepsilon = \Delta_x W_\varepsilon ~e^{u/\varepsilon},\\
+&\nabla_\theta n_\varepsilon = \nabla_\theta W_\varepsilon ~e^{u/\varepsilon} + W_\varepsilon ~e^{u/\varepsilon} \dfrac{\nabla_\theta u}{\varepsilon},\\
+&\Delta_\theta n_\varepsilon = \Delta_\theta W_\varepsilon ~e^{u/\varepsilon} + 2\nabla_\theta W_\varepsilon~e^{u/\varepsilon}~\dfrac{\nabla_\theta u}{\varepsilon} + W_\varepsilon e^{u/\varepsilon}\dfrac{|\nabla_\theta u|^2}{\varepsilon^2} + W_\varepsilon e^{u/\varepsilon}\dfrac{\Delta_\theta u}{\varepsilon}.
+\end{aligned}
+$$
+The model now reads as 
+$$
+\begin{aligned}
+&\varepsilon\partial_t W_\varepsilon e^{u/\varepsilon} + \color{red}{W_\varepsilon e^{u/\varepsilon}\partial_t u}\\
+&-D(\theta)\Delta_x W_\varepsilon ~e^{u/\varepsilon} -\varepsilon^2\left(\Delta_\theta W_\varepsilon ~e^{u/\varepsilon} + 2e^{u/\varepsilon}~\dfrac{\nabla_\theta W_\varepsilon\cdot \nabla_\theta u}{\varepsilon} + \color{red}{W_\varepsilon e^{u/\varepsilon}\dfrac{|\nabla_\theta u|^2}{\varepsilon^2} + W_\varepsilon e^{u/\varepsilon}\dfrac{\Delta_\theta u}{\varepsilon}}\right)\\
+& = W_\varepsilon e^{u/\varepsilon}(K(x)-\rho_\varepsilon(x)) \\
+& = W_\varepsilon e^{u/\varepsilon}(K(x)-\rho_\varepsilon(x)) + W_\varepsilon e^{u/\varepsilon} H^*(\theta) \color{red}{- W_\varepsilon e^{u/\varepsilon} H^*(\theta)}.
+\end{aligned}
+$$
+By properly separating the equation, we get
+$$
+	\partial_t u - |\nabla_\theta u|^2 - \varepsilon \Delta_\theta u = -H^*(\theta),
+$$
+and 
+$$
+\varepsilon\partial_t W_\varepsilon-D(\theta)\Delta_x W_\varepsilon -\varepsilon^2\Delta_\theta W_\varepsilon - 2\varepsilon\nabla_\theta W_\varepsilon\cdot \nabla_\theta u = W_\varepsilon (K(x)-\rho_\varepsilon(x)) + H^*(\theta) W,
+$$
+where $H^*(\theta)=H(\theta,\rho^*)$ is the approximation of $H(\theta, \rho_\varepsilon)$ with $\rho_\varepsilon = \int_0^1n_\varepsilon(x,\theta) d\theta $.
+
+
+
+
+## References
+1. *Rare Mutations Limit of a Steady State Dispersal Evolution Model*, B. Perthame and P. E. Souganidis, Math. Model. Nat. Phenom., 11(2016), 154-166.
+
--- code/main_bio.m
+++ code/main_bio.m
@@ -1,67 +1,61 @@
-function main_bio
 %% 网格参数   
-Nth = 20; % theta方向网格步长1/Nth
-Nx = 20; % x方向网格步长1/Nx
-theta = linspace(0, 1, Nth +1); % 网格点theta
-theta(end) = []; %周期边界，去掉theta(end)=1 
-x = linspace(0, 1, Nx + 1); % 网格点x  
-x = x'; % theta为行向量，x为列向量
-dtheta = theta(2) - theta(1); %theta步长
-dx = x(2) - x(1); %空间步长
+N_theta = 20; % theta方向网格步长1/Nth
+N_x = 20; % x方向网格步长1/Nx
+d_theta = 1 / N_theta;
+theta = 0:d_theta:1-d_theta;
+dx = 1 / N_x;
+x = (0:dx:1)'; % 网格点x  
 dt = 1e-2; %时间步长  
 T = 50; % 终止时间
 Ts = 1:50; % 存储Ts时刻的数据
 ks = 1; % 与Ts有关
   
 %% 插值准备
-[xx, thth] = meshgrid(x, [theta, 1]);
-thinte = 0:0.001:1; 
-[xx2, ththinte] = meshgrid(x, thinte);
+[X, Theta] = meshgrid(x, [theta, 1]);
+theta_f = 0:0.001:1; 
+[X_f, Theta_f] = meshgrid(x, theta_f);
  
 %% 问题参数  
 eps = 1e-2;       
 D = 0.5 * sin(pi * theta - pi) + 1;     
 K = 1 + 20 * (1 - 4 * (x - 0.5).^2).^8;  
-figure(10)  
-plot(theta, D);    
 
 %% 初值w(x,theta,t),u(theta,t) 以及初始化 H(\theta)
-w = ones(Nx+1, Nth); % theta \in [0, 1-dtheta]
+W = ones(N_x+1, N_theta); % theta \in [0, 1-dtheta]
 u = sin(pi * theta) - 1;
-H = zeros(1, Nth);
+H = zeros(1, N_theta);
 t = 0; 
 
-%% 某些准备部分，不放在循环里
+%% 准备部分，不放在循环里
 %% 一个theta对应一个D，对应1个特征值，so, 要么循环，要么三阶张量形式
 % 待定吧
-beta = eps^2 * dt / dtheta^2;
-B = (1 + 2*beta) * diag(ones(Nth, 1), 0) +...
-    - beta * diag(ones(Nth-1, 1), 1) +...
-    - beta *  diag(ones(Nth-1, 1), -1);
-B(1, Nth) = -beta;
-B(Nth, 1) = -beta;
+beta = eps^2 * dt / d_theta^2;
+B = (1 + 2*beta) * diag(ones(N_theta, 1), 0) +...
+    - beta * diag(ones(N_theta-1, 1), 1) +...
+    - beta *  diag(ones(N_theta-1, 1), -1);
+B(1, N_theta) = -beta;
+B(N_theta, 1) = -beta;
 % 二阶导隐式，系数矩阵  
 alpha = dt .* D ./ dx^2;
 alpha = reshape(alpha, 1, 1, []);
-A_pre = diag(ones(Nx-1, 1), 0) .* (1+2*alpha+2*beta) + ...
-    - diag(ones(Nx-2, 1), 1) .* alpha + ...
-    - diag(ones(Nx-2, 1), -1) .* alpha; 
+A_pre = diag(ones(N_x-1, 1), 0) .* (1+2*alpha+2*beta) + ...
+    - diag(ones(N_x-2, 1), 1) .* alpha + ...
+    - diag(ones(N_x-2, 1), -1) .* alpha; 
 A_pre(1, 1, :) = 1+alpha+2 * beta;
 A_pre(end, end, :) = 1+alpha+2 * beta;
-A_sub = -beta .* diag(ones(Nx-1, 1), 0);
+A_sub = -beta .* diag(ones(N_x-1, 1), 0);
 A_cell = squeeze(num2cell(A_pre, [1, 2]));
 A_con = blkdiag(A_cell{:});
-A_con = A_con + kron(diag(ones(Nth-1, 1), 1), A_sub) + kron(diag(ones(Nth-1, 1), -1), A_sub);
-A_con(end-Nx+2:end, 1:Nx-1) = A_sub;
-A_con(1:Nx-1, end-Nx+2:end) = A_sub;
+A_con = A_con + kron(diag(ones(N_theta-1, 1), 1), A_sub) + kron(diag(ones(N_theta-1, 1), -1), A_sub);
+A_con(end-N_x+2:end, 1:N_x-1) = A_sub;
+A_con(1:N_x-1, end-N_x+2:end) = A_sub;
 path = './data/';
 %% 时间演化
 while t <= T  
     
-    if abs(t-Ts(ks)) <10^(-7)
-        save(strcat(path, 'u_', num2str(eps), '_', num2str(t), '_', num2str(Nx), '_', num2str(Nth), '.mat'), 'u');
-        save(strcat(path, 'w_', num2str(eps), '_', num2str(t), '_', num2str(Nx), '_', num2str(Nth), '.mat'), 'w');
-        ks = ks + 1;
+    if min(abs(t-Ts)) < dt/10
+        save(strcat(path, 'u_', num2str(eps), '_', num2str(t), '_', num2str(N_x), '_', num2str(N_theta), '.mat'), 'u');
+        save(strcat(path, 'W_', num2str(eps), '_', num2str(t), '_', num2str(N_x), '_', num2str(N_theta), '.mat'), 'W');
     end
     %% rho,积分，数值积分，随着epsilon的减小，
     %% 这个数值积分不晓得会不会有问题，精度也许达不到。rho与x有关，与theta无关
@@ -69,43 +63,44 @@ while t <= T
  %   rho1 = dtheta * sum(w(:, 1:Nth) .* exp(u(1:Nth)/eps), 2);
     %% 插值
     u_aux = [u, u(:, 1)];
-    w_aux = [w, w(:, 1)];
-    uinte = interp1([theta, 1], u_aux, thinte, 'spline');
-    winte = interp2(xx, thth, w_aux', xx2, ththinte, 'spline');
+    w_aux = [W, W(:, 1)];
+    uinte = interp1([theta, 1], u_aux, theta_f, 'spline');
+    winte = interp2(X, Theta, w_aux', X_f, Theta_f, 'spline');
     winte = winte';   
-    rho = (thinte(2)-thinte(1)) * sum(winte(:, 1:end-1) .* exp(uinte(1:end-1)/eps), 2);
+    rho = (theta_f(2)-theta_f(1)) * sum(winte(:, 1:end-1) .* exp(uinte(1:end-1)/eps), 2);
       
     %% 画图   
     figure(1);    
     plot(x, rho);
+    title(['Plot of $\rho(x)$, time = ', num2str(t)], 'Interpreter', 'latex');
     %% 画图  
     figure(2);  
     plot(theta, u);  
- %   axis([0 1 0 2]);   
- %   pause(0.001); 
+    title(['Plot of $u(\theta)$, time = ', num2str(t)], 'Interpreter', 'latex');
     
     %% 特征值问题。系数矩阵C表示， D(theta),
     %% 一个theta对应一个D，对应1个特征值，so, 循环
-    Tri_C = - 2 * diag(ones(Nx-1, 1), 0) +...
-            + diag(ones(Nx-2, 1), 1) +...
-            + diag(ones(Nx-2, 1), -1);  
+    Tri_C = - 2 * diag(ones(N_x-1, 1), 0) +...
+            + diag(ones(N_x-2, 1), 1) +...
+            + diag(ones(N_x-2, 1), -1);  
     Tri_C(1, 1) = -1;  
     Tri_C(end, end) = -1;
-    for kk = 1: Nth % 计算特征值
-        C = - D(kk) ./ dx^2 .* Tri_C - diag(K(2:Nx) - rho(2: Nx));
+    for kk = 1: N_theta % 计算特征值
+        C = - D(kk) ./ dx^2 .* Tri_C - diag(K(2:N_x) - rho(2: N_x));
         H(kk) = eigs(C, 1, 'smallestreal');
     end    
     
     figure(3)
     plot(theta, H);
-    
+    title(['Plot of $H(\theta)$, time = ', num2str(t)], 'Interpreter', 'latex');
+
     %% solve the second equation to obtain u(theta)，迎风格式？
     u_plus = [u(2:end), u(1)]; % 关于theta是周期边界
     u_minus = [u(end), u(1:end-1)];
     % 非线性项的正负处理：信息p^2从左来，即一阶导p>0,则左差分/后向差分；否则右差分
-    pL = (u - u_minus) / dtheta; % 左差分 u_j - u_{j-1}
-    pR = (u_plus - u) / dtheta; % 右差分 u_{j+1} - u_j
-    pC = (u_plus - u_minus) / (2*dtheta);
+    pL = (u - u_minus) / d_theta; % 左差分 u_j - u_{j-1}
+    pR = (u_plus - u) / d_theta; % 右差分 u_{j+1} - u_j
+    pC = (u_plus - u_minus) / (2*d_theta);
     grad_sq = pL.^2 .* (pC >= 0) + pR.^2 .* (pC < 0);
     % 二阶导用隐式，一阶导用迎风,隐式Euler
     u_new = B\(u + dt * eps * (grad_sq - H))';  
@@ -115,35 +110,34 @@ while t <= T
     u = u_new'; % 更新  
     u_plus = [u(2:end), u(1)];
     u_minus = [u(end), u(1:end-1)];
-    dudtheta = (u_plus - u_minus) / (2*dtheta);
+    dudtheta = (u_plus - u_minus) / (2*d_theta);
   %
-    w_theta_plus = w(:, [2:end, 1]); % 关于theta是周期边界条件
-    w_theta_minus = w(:, [end, 1:end-1]);
-    dw_dtheta_forward = (w_theta_plus - w) / dtheta; % 右差分，dudtheta<0，
-    dw_dtheta_backward = (w - w_theta_minus) / dtheta; % 左差分, dudtheta>0
+    w_theta_plus = W(:, [2:end, 1]); % 关于theta是周期边界条件
+    w_theta_minus = W(:, [end, 1:end-1]);
+    dw_dtheta_forward = (w_theta_plus - W) / d_theta; % 右差分，dudtheta<0，
+    dw_dtheta_backward = (W - w_theta_minus) / d_theta; % 左差分, dudtheta>0
     dw_dtheta_upwind = dw_dtheta_backward .* (dudtheta >= 0) +...
                      + dw_dtheta_forward .* (dudtheta <0);
     
     coupling = 2 * eps * (dw_dtheta_upwind .* dudtheta);
     
     %%%系数矩阵
-    A_diag2 = diag(K(2:Nx) - rho(2:Nx), 0); 
+    A_diag2 = diag(K(2:N_x) - rho(2:N_x), 0); 
     
-    H_mat = reshape(H, [1, 1, Nth]);
-    A_H_cell = squeeze(num2cell(H_mat.*eye(Nx-1), [1, 2]));
+    H_mat = reshape(H, [1, 1, N_theta]);
+    A_H_cell = squeeze(num2cell(H_mat.*eye(N_x-1), [1, 2]));
     A_H = blkdiag(A_H_cell{:});
-    A = A_con - dt * kron(diag(ones(Nth, 1), 0), A_diag2) - dt * A_H;
+    A = A_con - dt * kron(diag(ones(N_theta, 1), 0), A_diag2) - dt * A_H;
 
     % 右端项  
-    b = w + dt * (coupling); 
-    b = reshape(b(2: Nx, :), [], 1);
+    b = W + dt * (coupling); 
+    b = reshape(b(2: N_x, :), [], 1);
     %向前Euler         
     w_new = A \ b;      
-    w = reshape(w_new, size(w(2:Nx, :)));% 更新
-    w = [w(1, :); w; w(end, :)];
+    W = reshape(w_new, size(W(2:N_x, :)));% 更新
+    W = [W(1, :); W; W(end, :)];
     t = t+dt;  
     
 end
 
-end
 
