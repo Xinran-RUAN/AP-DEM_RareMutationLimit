3c676969fb6253367a0464c47c54166d5b550d10
Xinran_RUAN <ruanxinran@me.com>
2025-07-27 12:38:39 +0800

    Refactor WENO5_left and WENO5_right for consistency
    Standardized array concatenation to use column vectors, initialized all intermediate variables, and corrected index assignments in WENO5_right to match the left-biased version. Also fixed the weights and smoothness indicators for the right-biased reconstruction to ensure correct WENO5 implementation.

--- code/WENO5_right.m
+++ code/WENO5_right.m
@@ -1,20 +1,19 @@
 function [pR] = WENO5_right(u, dx)
     eps0 = 1e-6;
-    pR = zeros(size(u));
     N = length(u);
-    u = [u(end-2:end), u, u(1:3)];
-    % q0 = zeros(N, 1);
-    % q1 = zeros(N, 1);
-    % q2 = zeros(N, 1);
-    
+    u = [u(end-2:end); u; u(1:3)];
+    q0 = zeros(N, 1);
+    q1 = zeros(N, 1);
+    q2 = zeros(N, 1);
+    pR = zeros(N,1);
     for j = 1:N
         jj = j + 3;
-        vppp = u(jj+3);
-        vpp = u(jj+2);
-        vp  = u(jj+1);
+        vmmm = u(jj+3);
+        vmm = u(jj+2);
+        vm  = u(jj+1);
         v   = u(jj);
-        vm  = u(jj-1);
-        vmm = u(jj-2);
+        vp  = u(jj-1);
+        vpp = u(jj-2);
 
         %% 以下为点值的重构，而非单侧导数的重构
         % 三个候选 
@@ -38,33 +37,30 @@ function [pR] = WENO5_right(u, dx)
 
         % pR(j) = (w0 * q0(j) + w1 * q1(j) + w2 * q2(j)) / dx;
         %% 单侧导数的重构
-        a = vppp - 2 * vpp + vp;
-        b = vpp - 2 * vp + v;
-        c = vp - 2 * v + vm;
-        d = v - 2 * vm + vmm;
+        a = vmmm - 2 * vmm + vm;
+        b = vmm - 2 * vm + v;
+        c = vm - 2 * v + vp;
+        d = v - 2 * vp + vpp;
 
         IS0 = 13 * (a - b)^2 + 3 * (a - 3 * b)^2;
         IS1 = 13 * (b - c)^2 + 3 * (b + c)^2;
         IS2 = 13 * (c - d)^2 + 3 * (3 * c - d)^2;
 
-        a0 = 1 / (eps0 + IS0)^2;
+        a0 = 3 / (eps0 + IS0)^2;
         a1 = 6 / (eps0 + IS1)^2;
-        a2 = 3 / (eps0 + IS2)^2;
+        a2 = 1 / (eps0 + IS2)^2;
         wsum = a0 + a1 + a2;
         w0 = a0 / wsum;
         % w1 = a1 / wsum;
         w2 = a2 / wsum;
 
         pR(j) = (...
-            - (vm - vmm) ...
-            + 7 * (v - vm) ...
-            + 7 * (vp - v) ...
-            - (vpp - vp)...
+            - (vp - vpp) ...
+            + 7 * (v - vp) ...
+            + 7 * (vm - v) ...
+            - (vmm - vm)...
             ) / 12 ...
-            + (...
-            w0 * (a - 2 * b + c) / 3 ...
-            + (w2 - 1/2) * (b - 2 * c + d) / 6 ...
-        );
+            + w0 * (a - 2 * b + c) / 3 + (w2 - 1/2) * (b - 2 * c + d) / 6;
         pR(j) = pR(j) / dx;
     end
 end
